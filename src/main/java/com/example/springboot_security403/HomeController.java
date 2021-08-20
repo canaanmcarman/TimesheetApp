a76a4b9a8fbb6c91ab80ae991f386e98d57d8106
canaanmcarman <canaan.m.carman@gmail.com>
2021-08-20 00:38:01 -0400

    manual mergin with ruth

--- src/main/java/com/example/springboot_security403/HomeController.java
+++ src/main/java/com/example/springboot_security403/HomeController.java
@@ -41,7 +41,7 @@ public class HomeController {
     }
 
     @RequestMapping("/timesheetapproval/{id}")
-    public String processTimesheet(@PathVariable("id") long id, Model model) {
+    public String processTimesheet(@PathVariable("id") long id, Model model, Principal principal) {
         Timesheet timesheet = timesheetRepository.findById(id).get();
         //check if already sent for approval
         if (timesheet.getStage().equals("pending approval")) {
@@ -56,11 +56,12 @@ public class HomeController {
         model.addAttribute("timesheet", timesheet);
         //email section
         //for admin
+        String username = principal.getName();
         String header = " header ....";
-        String contentForAdmin = "Employee has submitted a timesheet for review. Please check the queue.";
+        String contentForAdmin = username + " has submitted a timesheet for review. Please check the queue.";
         String emailAdmin = "ccamaru89@gmail.com";
         //for employee
-        String contentForEmployee = "Hi Employee, your timesheet was submitted and is pending approval";
+        String contentForEmployee = "Hi," + username + " your timesheet was submitted and is pending approval";
         User employee = timesheet.getEmployee();
         String emailEmployee = employee.getEmail();
         emailService.sendSimpleEmail(contentForAdmin, header, emailAdmin);
@@ -71,6 +72,7 @@ public class HomeController {
         String message = "Submitted for approval! Check your email.";
         model.addAttribute("message3", message);
         return "timesheet";
+
     }
 
 
@@ -89,15 +91,30 @@ public class HomeController {
         return "timesheet";
     }
     @RequestMapping("/approve/{id}")
-    public String approveTimesheet(@PathVariable("id") long id, Model model) {
+    public String approveTimesheet(@PathVariable("id") long id, Model model, Principal principal) {
         Timesheet timesheet = timesheetRepository.findById(id).get();
         timesheet.setStage("approved");
         model.addAttribute("timesheet", timesheet);
         timesheetRepository.save(timesheet);
 
+        //ruth changes
+        String username = principal.getName();
+        String header = " header ....";
+        User employee = timesheet.getEmployee();
+        String emailEmployee = employee.getEmail();
+        //for employee
+        String contentForEmployee = "Hi " + username + " your timesheet is just approved" + "\n" + "\n" + "\n"
+                + "Employee Name: " + timesheet.getEmployee().getFirstName() + " " + timesheet.getEmployee().getLastName() + "\n"
+                + "Gross Earning: " + timesheet.getWeekTotal() + "\n"
+                + "Hours Worked: " + timesheet.getRegular() + "\n";
+//                + "Over Time worked: "+ timesheet.getOvertimeHours();
+
+        emailService.sendSimpleEmail(contentForEmployee, header, emailEmployee);
+
         // send user a paystub
 
-        return "index";
+        return "paystub";
+
     }
 
     @RequestMapping("/reject/{id}")
